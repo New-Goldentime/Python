978bdf0111642ae500a60828f5f52dc6198fb793
Mark <clearcoder1@gmail.com>
2023-01-23 15:32:20 +0900

    retry

--- django_auth/venv/django_project/django_project/settings.py
+++ django_auth/venv/django_project/django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ey^k*tia(4#_b^ey5x(1v_wdh2nac_p*&is&$wyx@f^%v5y%ja'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
